748cbcf6c93b9b13bff6b97259eb108c0c6961da
Lalith <lalithpunepalli@gmail.comm>
2020-10-11 23:16:40 +0530

    production build done

---
+++ build/precache-manifest.50982bc290361e6943e0fdc9b5d94064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf033d90abfc4ebdff3fbbda1444554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a71b3211af826105368",
+    "url": "/static/js/2.06301d5b.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.06301d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80668a79e0a19b4786f7",
+    "url": "/static/js/main.db5956b6.chunk.js"
+  },
+  {
+    "revision": "b15eaae287ec8d6296da",
+    "url": "/static/js/runtime-main.09fa4e7a.js"
+  }
+]);
